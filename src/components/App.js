450b20db0b4864ddab260bc133db56bfacd37ee5
mZubeldia <mzubeldia09@gmail.com>
2021-07-08 21:20:55 +0200

    create nueva ruta task form

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,62 @@ const App = () => {
     <>
       <header className="header">
         <h1 className="header-text">Truck delivery</h1>
+        <p>¡Hola!</p>
       </header>
+
+      <main className="main">
+        <section className="task-section">
+          <h2>Nueva tarea</h2>
+
+          <form className="task-form">
+            <fieldset className="task-form__route">
+              <legend>Nueva ruta</legend>
+              <label className="task-form__route-item">
+                Granja
+                <select name="farm">
+                  <option value="1">Granja 1</option>
+                  <option value="2">Granja 2</option>
+                  <option value="3">Granja 3</option>
+                </select>
+              </label>
+
+              <label className="task-form__route-item">
+                Tipo de camión
+                <select name="truck">
+                  <option value="sm">Camión 1</option>
+                  <option value="md">Camión 2</option>
+                  <option value="bg">Camión 3</option>
+                </select>
+              </label>
+
+              <label className="task-form__route-item">
+                Tipo de carga
+                <label className="task-form__route-item">
+                  Nº de cerdos
+                  <input type="number" />
+                </label>
+                <label>
+                  Tipo de cerdos
+                  <select name="pigs">
+                    <option value="w">Tipo 1</option>
+                    <option value="b">Tipo 2</option>
+                    <option value="d">Tipo 3</option>
+                  </select>
+                </label>
+              </label>
+            </fieldset>
+
+            <fieldset>
+              <legend>Fecha</legend>
+              <label className="task-form__route-item">
+                Fecha de la tarea
+                <input type="date" />
+              </label>
+            </fieldset>
+            <button className="task-btn">Crear</button>
+          </form>
+        </section>
+      </main>
 
       <footer className="footer">
         <small className="footer__rights">
