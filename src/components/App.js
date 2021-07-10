ff3b80b20dd2060882d595d2c063a8631913bac9
mZubeldia <mzubeldia09@gmail.com>
2021-07-10 16:54:47 +0200

    create component for CreateFarm html and css styles

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import "../stylesheet/reset.scss";
 import Header from "./Header";
 import Footer from "./Footer";
 import CreateTask from "./CreateTask";
+import CreateFarm from "./CreateFarm";
 
 //servicios
 //import getApiData from "../services/api";
@@ -19,52 +20,7 @@ const App = () => {
     <>
       <Header />
       {/*<CreateTask />*/}
-      <main className="main">
-        <h2>Crear granja</h2>
-        <form action="" method="#" className="farm-form form">
-          <div className="left-side">
-            <label htmlFor="new-farm" className="form__label">
-              Nueva granja
-              <input
-                className="form-item"
-                type="text"
-                name="new-farm"
-                placeholder="Granja Pepe"
-              />
-            </label>
-            <label htmlFor="location" className="form__label">
-              Geolocalización
-              <input
-                className="form-item"
-                type="text"
-                name="location"
-                placeholder="1.111.111"
-              />
-            </label>
-            <label htmlFor="phone" className="form__label">
-              Nueva granja
-              <input
-                className="form-item"
-                type="text"
-                name="phone"
-                placeholder="656 111 223"
-              />
-            </label>
-          </div>
-          <div className="right-side">
-            <label htmlFor="others" className="form__label">
-              Comentarios adicionales
-              <input
-                className="form-item others"
-                type="textarea"
-                name="others"
-                placeholder="Tipo de entrada, hora de apertura, etc."
-              />
-            </label>
-          </div>
-          <button className="btn-farm">Crear</button>
-        </form>
-      </main>
+      <CreateFarm />
       <Footer />
     </>
   );
