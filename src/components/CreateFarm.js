1e7d1ec959de59cc7e68756753340ba93a83dd16
mZubeldia <mzubeldia09@gmail.com>
2021-07-10 17:09:22 +0200

    change styles for createFarm component

--- src/components/CreateFarm.js
+++ src/components/CreateFarm.js
@@ -3,46 +3,42 @@ const CreateFarm = () => {
     <main className="main">
       <h2>Crear granja</h2>
       <form action="" method="#" className="farm-form form">
-        <div className="left-side">
-          <label htmlFor="new-farm" className="form__label">
-            Nueva granja
-            <input
-              className="form-item"
-              type="text"
-              name="new-farm"
-              placeholder="Granja Pepe"
-            />
-          </label>
-          <label htmlFor="location" className="form__label">
-            Geolocalización
-            <input
-              className="form-item"
-              type="text"
-              name="location"
-              placeholder="1.111.111"
-            />
-          </label>
-          <label htmlFor="phone" className="form__label">
-            Nueva granja
-            <input
-              className="form-item"
-              type="text"
-              name="phone"
-              placeholder="656 111 223"
-            />
-          </label>
-        </div>
-        <div className="right-side">
-          <label htmlFor="others" className="form__label">
-            Comentarios adicionales
-            <input
-              className="form-item others"
-              type="textarea"
-              name="others"
-              placeholder="Tipo de entrada, hora de apertura, etc."
-            />
-          </label>
-        </div>
+        <label htmlFor="new-farm" className="form__label">
+          Nueva granja
+          <input
+            className="form-item"
+            type="text"
+            name="new-farm"
+            placeholder="Granja Pepe"
+          />
+        </label>
+        <label htmlFor="location" className="form__label">
+          Geolocalización
+          <input
+            className="form-item"
+            type="text"
+            name="location"
+            placeholder="1.111.111"
+          />
+        </label>
+        <label htmlFor="phone" className="form__label">
+          Nueva granja
+          <input
+            className="form-item"
+            type="text"
+            name="phone"
+            placeholder="656 111 223"
+          />
+        </label>
+        <label htmlFor="others" className="form__label">
+          Comentarios adicionales
+          <input
+            className="form-item others"
+            type="textarea"
+            name="others"
+            placeholder="Tipo de entrada, hora de apertura, etc."
+          />
+        </label>
         <button className="btn-farm">Crear</button>
       </form>
     </main>
