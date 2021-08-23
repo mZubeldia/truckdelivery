88a494a11b78e056d7cc84e75265d6189e0bc81a
mZubeldia <mzubeldia09@gmail.com>
2021-08-23 19:20:42 +0200

    add hidden classes to dates and mediaq in landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,29 @@ const Home = () => {
         <i class="fas fa-clipboard-list"></i> <i class="far fa-clock"></i>
       </h2>
       <ul className="main__list">
-        <li className="main__list__item">Ruta 1</li>
-        <li className="main__list__item">Ruta 2</li>
-        <li className="main__list__item">Ruta 3</li>
+        <li className="main__list__item">
+          Ruta 1 <div class="hidden">23/oct/21</div>
+        </li>
+        <li className="main__list__item">
+          Ruta 2 <div class="hidden">24/oct/21</div>
+        </li>
+        <li className="main__list__item">
+          Ruta 3 <div class="hidden">28/nov/21</div>
+        </li>
       </ul>
       <h2>
         <i class="fas fa-clipboard-list"></i> <i class="fas fa-check"></i>
       </h2>
       <ul className="main__list">
-        <li className="main__list__item">Ruta A</li>
-        <li className="main__list__item">Ruta B</li>
-        <li className="main__list__item">Ruta C</li>
+        <li className="main__list__item">
+          Ruta A <div class="hidden">14/apr/21</div>
+        </li>
+        <li className="main__list__item">
+          Ruta B <div class="hidden">28/may/21</div>
+        </li>
+        <li className="main__list__item">
+          Ruta C <div class="hidden">28/dec/20</div>
+        </li>
       </ul>
     </main>
   );
