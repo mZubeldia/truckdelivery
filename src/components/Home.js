dc73cd9660157317f8af49db5a5afbf388fc161e
mZubeldia <mzubeldia09@gmail.com>
2021-08-23 19:01:29 +0200

    add class names to landing css

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,18 @@ const Home = () => {
       <h2>
         <i class="fas fa-clipboard-list"></i> <i class="far fa-clock"></i>
       </h2>
-      <ul>
-        <li>Ruta 1</li>
-        <li>Ruta 2</li>
-        <li>Ruta 3</li>
+      <ul className="main__list">
+        <li className="main__list__item">Ruta 1</li>
+        <li className="main__list__item">Ruta 2</li>
+        <li className="main__list__item">Ruta 3</li>
       </ul>
       <h2>
         <i class="fas fa-clipboard-list"></i> <i class="fas fa-check"></i>
       </h2>
-      <ul>
-        <li>Ruta A</li>
-        <li>Ruta B</li>
-        <li>Ruta C</li>
+      <ul className="main__list">
+        <li className="main__list__item">Ruta A</li>
+        <li className="main__list__item">Ruta B</li>
+        <li className="main__list__item">Ruta C</li>
       </ul>
     </main>
   );
