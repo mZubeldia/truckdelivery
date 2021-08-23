55e60de89852e6dcc72ad5a67961c6fc10cf3738
mZubeldia <mzubeldia09@gmail.com>
2021-08-23 20:08:36 +0200

    fix style nueva granja mobile

--- src/components/CreateFarm.js
+++ src/components/CreateFarm.js
@@ -7,57 +7,53 @@ const CreateFarm = () => {
         Volver{" "}
       </Link>
 
-      <h2 className="main__farm__title">Crear granja</h2>
+      <h2 className="main__farm__title">Nueva granja</h2>
       <form action="" method="#" className="row g-3 farm-form">
         <div className="row">
-          <div className="col">
-            <div className="mb-3">
-              <label htmlFor="new-farm" className="form-label">
-                Nueva granja
-                <input
-                  className="form-control"
-                  type="text"
-                  name="new-farm"
-                  placeholder="Granja Pepe"
-                />
-              </label>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="location" className="form-label">
-                Geolocalización
-                <input
-                  className="form-control"
-                  type="text"
-                  name="location"
-                  placeholder="1.111.111"
-                />
-              </label>
-            </div>
+          <div className="mb-3">
+            <label htmlFor="new-farm" className="form-label">
+              Nombre
+              <input
+                className="form-control"
+                type="text"
+                name="new-farm"
+                placeholder="Granja Pepe"
+              />
+            </label>
           </div>
-          <div className="col">
-            <div className="mb-3">
-              <label htmlFor="phone" className="form-label ">
-                Nueva granja
-                <input
-                  className="form-control"
-                  type="text"
-                  name="phone"
-                  placeholder="656 111 223"
-                />
-              </label>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="others" className="form-label mb-3">
-                Comentarios adicionales
-                <textarea
-                  className="form-control"
-                  type="textarea"
-                  name="others"
-                  rows="3"
-                  placeholder="Tipo de entrada, hora de apertura, etc."
-                />
-              </label>
-            </div>
+          <div className="mb-3">
+            <label htmlFor="location" className="form-label">
+              Geolocalización
+              <input
+                className="form-control"
+                type="text"
+                name="location"
+                placeholder="1.111.111"
+              />
+            </label>
+          </div>
+
+          <label htmlFor="phone" className="form-label phone">
+            Telefono
+            <input
+              className="form-control"
+              type="text"
+              name="phone"
+              placeholder="656 111 223"
+            />
+          </label>
+
+          <div className="mb-3">
+            <label htmlFor="others" className="form-label">
+              Comentarios
+              <textarea
+                className="form-control"
+                type="textarea"
+                name="others"
+                rows="3"
+                placeholder="Tipo de entrada, hora de apertura, etc."
+              />
+            </label>
           </div>
         </div>
 
