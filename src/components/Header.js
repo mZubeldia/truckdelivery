e5e524359665adb5c61b0744541677ce3ad0fdd8
mZubeldia <mzubeldia09@gmail.com>
2021-08-23 18:43:34 +0200

    add icons for add, farm and task

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,12 @@ const Header = () => {
         <ul className="nav ">
           <li className="nav-item">
             <Link to="/nueva-granja" className="nav-link ">
-              Crear una nueva granja
+              <i class="fas fa-plus"></i> <i class="fas fa-tractor"></i>
             </Link>
           </li>
           <li className="nav-item">
             <Link to="/nueva-tarea" className="nav-link">
-              Crear una nueva tarea
+              <i class="fas fa-plus"></i> <i class="fas fa-clipboard-list"></i>
             </Link>
           </li>
           <li className="nav-item">
