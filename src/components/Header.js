e154b9e141efd70b5cb82ac09c3685f5547a64f2
mZubeldia <mzubeldia09@gmail.com>
2021-08-24 13:33:59 +0200

    add first styles and structure burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header-text">
-        <a href="/">Truck Delivery</a>
-      </h1>
-      <nav>
-        <ul className="nav ">
+      <nav className="navbar">
+        <h1 className="header-text">
+          <a href="/">Truck Delivery</a>
+        </h1>
+
+        <ul className="nav-menu">
           <li className="nav-item">
             <Link to="/nueva-granja" className="nav-link ">
               <i class="fas fa-plus"></i> <i class="fas fa-tractor"></i>
@@ -18,6 +19,13 @@ const Header = () => {
             </Link>
           </li>
         </ul>
+        <div className="hamburger">
+          <button type="button" className="hamburger-btn">
+            <span className="hamburger-bar"></span>
+            <span className="hamburger-bar"></span>
+            <span className="hamburger-bar"></span>
+          </button>
+        </div>
       </nav>
     </header>
   );
