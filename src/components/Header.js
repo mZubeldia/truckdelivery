5062fd95962c31ff946c29b2b7d0a1b8c1acf36a
mZubeldia <mzubeldia09@gmail.com>
2021-08-25 18:49:21 +0200

    delete styles and comments old burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,31 +7,8 @@ const Header = () => {
         <h1 className="header-text">
           <a href="/">Truck Delivery</a>
         </h1>
-        {/* 
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <Link to="/nueva-granja" className="nav-link ">
-              <i class="fas fa-plus"></i> <i class="fas fa-tractor"></i>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/nueva-tarea" className="nav-link">
-              <i class="fas fa-plus"></i> <i class="fas fa-clipboard-list"></i>
-            </Link>
-          </li>
-        </ul>
-*/}
 
         <Burguer />
-        {/*
-        <div className="hamburger">
-          <button type="button" className="hamburger-btn">
-            <span className="hamburger-bar"></span>
-            <span className="hamburger-bar"></span>
-            <span className="hamburger-bar"></span>
-          </button>
-        </div>
-        */}
       </nav>
     </header>
   );
