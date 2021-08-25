d3838bf4162192cfb2062f00b0e49e5930ad3beb
mZubeldia <mzubeldia09@gmail.com>
2021-08-25 18:53:25 +0200

    add name to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ const Footer = () => {
     <footer className="footer">
       <small className="footer__rights">
         <span className="rights__item">&copy;2021</span>
+        <span className="rights__text">Truck Delivery</span>
       </small>
     </footer>
   );
