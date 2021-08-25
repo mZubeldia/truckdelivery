770f5dd0eaf12d151d9ac331bcaf299a6b5d7149
mZubeldia <mzubeldia09@gmail.com>
2021-08-25 17:42:17 +0200

    add div and basic styles for todays tasks in landing

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,23 @@ const Home = () => {
   return (
     <main className="main main-landing">
       <ul className="main__list pending">
-        <i class="far fa-clock pending-icon landing-icon"></i>
-
+        <div class="today-wrapper">
+          <div className="landing-title-wrapper">
+            <i class="far fa-clock pending-icon landing-icon"></i>
+            <div class="title-text-wrapper">
+              <h2 className="landing-title-text">Hoy</h2> <span>25 agosto</span>
+            </div>
+          </div>
+          <li className="main__list__item">
+            Ruta 1 <div class="hidden">Granja Norte</div>
+          </li>
+          <li className="main__list__item">
+            Ruta 2 <div class="hidden">Granja Sur</div>
+          </li>
+        </div>
         <li className="main__list__item">
-          Ruta 1 <div class="hidden">23/oct/21</div>
-        </li>
-        <li className="main__list__item">
-          Ruta 2 <div class="hidden">24/oct/21</div>
-        </li>
-        <li className="main__list__item">
-          Ruta 3 <div class="hidden">28/nov/21</div>
+          Ruta 3 <div class="hidden">Granja Norte</div>{" "}
+          <div class="hidden">28 sept</div>
         </li>
       </ul>
 
