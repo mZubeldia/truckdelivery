64c5be9fdc4d8f67fea2d922309f3369ff48043c
mZubeldia <mzubeldia09@gmail.com>
2021-08-25 17:17:06 +0200

    add color to fonts in landing

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <main className="main main-landing">
       <ul className="main__list pending">
-        <i class="far fa-clock pending-icon"></i>
+        <i class="far fa-clock pending-icon landing-icon"></i>
 
         <li className="main__list__item">
           Ruta 1 <div class="hidden">23/oct/21</div>
@@ -16,7 +16,7 @@ const Home = () => {
       </ul>
 
       <ul className="main__list done">
-        <i class="fas fa-check done-icon"></i>
+        <i class="fas fa-check done-icon landing-icon"></i>
 
         <li className="main__list__item">
           Ruta A <div class="hidden">14/apr/21</div>
