8b3a944311e34225dbc09bfd7026fc0a036dd30f
mZubeldia <mzubeldia09@gmail.com>
2021-08-26 13:27:13 +0200

    fix semantic and css styles for pending section in landing

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,20 +9,22 @@ const Home = () => {
               <h2 className="landing-title-text">Hoy</h2> <span>25 agosto</span>
             </div>
           </div>
+
           <li className="main__list__item">
-            <a href="#">
+            <a href="#" className="pending-link">
               Ruta 1 <div class="hidden">Granja Norte</div>
             </a>
           </li>
           <li className="main__list__item">
-            <a href="#">
+            <a href="#" className="pending-link">
               Ruta 2 <div class="hidden">Granja Sur</div>
             </a>
           </li>
         </ul>
+
         <ul class="pending-wrapper">
-          <li className="main__list__item">
-            <a href="#">
+          <li className="main__list__item pending__item">
+            <a href="#" className="pending-link">
               Ruta 3 <div class="hidden">Granja Norte</div>{" "}
               <div class="hidden">28 sept</div>
             </a>
@@ -33,15 +35,23 @@ const Home = () => {
       <section className="main__list done">
         <i class="fas fa-check done-icon landing-icon"></i>
 
-        <li className="main__list__item">
-          Ruta A <div class="hidden">14/apr/21</div>
-        </li>
-        <li className="main__list__item">
-          Ruta B <div class="hidden">28/may/21</div>
-        </li>
-        <li className="main__list__item">
-          Ruta C <div class="hidden">28/dec/20</div>
-        </li>
+        <ul class="done-wrapper">
+          <li className="main__list__item">
+            <a href="" className="done-link">
+              Ruta A <div class="hidden">14/apr/21</div>
+            </a>
+          </li>
+          <li className="main__list__item">
+            <a href="" className="done-link">
+              Ruta B <div class="hidden">28/may/21</div>
+            </a>
+          </li>
+          <li className="main__list__item">
+            <a href="" className="done-link">
+              Ruta C <div class="hidden">28/dec/20</div>
+            </a>
+          </li>
+        </ul>
       </section>
     </main>
   );
