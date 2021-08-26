148075f502eb9c61cba8522ab1e3cfabc16e45a2
mZubeldia <mzubeldia09@gmail.com>
2021-08-26 17:42:25 +0200

    center component nueva granja

--- src/components/CreateFarm.js
+++ src/components/CreateFarm.js
@@ -9,29 +9,25 @@ const CreateFarm = () => {
 
       <h2 className="main__farm__title">Nueva granja</h2>
       <form action="" method="#" className="row g-3 farm-form">
-        <div className="row">
-          <div className="mb-3">
-            <label htmlFor="new-farm" className="form-label">
-              Nombre
-              <input
-                className="form-control input"
-                type="text"
-                name="new-farm"
-                placeholder="Granja Pepe"
-              />
-            </label>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="location" className="form-label">
-              Geolocalización
-              <input
-                className="form-control input"
-                type="text"
-                name="location"
-                placeholder="1.111.111"
-              />
-            </label>
-          </div>
+        <div className="row form-container">
+          <label htmlFor="new-farm" className="form-label">
+            Nombre
+            <input
+              className="form-control input"
+              type="text"
+              name="new-farm"
+              placeholder="Granja Pepe"
+            />
+          </label>
+          <label htmlFor="location" className="form-label">
+            Geolocalización
+            <input
+              className="form-control input"
+              type="text"
+              name="location"
+              placeholder="1.111.111"
+            />
+          </label>
 
           <label htmlFor="phone" className="form-label phone">
             Telefono
@@ -43,18 +39,16 @@ const CreateFarm = () => {
             />
           </label>
 
-          <div className="mb-3">
-            <label htmlFor="others" className="form-label">
-              Comentarios
-              <textarea
-                className="form-control input"
-                type="textarea"
-                name="others"
-                rows="3"
-                placeholder="Tipo de entrada, hora de apertura, etc."
-              />
-            </label>
-          </div>
+          <label htmlFor="others" className="form-label">
+            Comentarios
+            <textarea
+              className="form-control input"
+              type="textarea"
+              name="others"
+              rows="3"
+              placeholder="Tipo de entrada, hora de apertura, etc."
+            />
+          </label>
         </div>
 
         <button className="btn-farm btn btn-primary">Crear</button>
